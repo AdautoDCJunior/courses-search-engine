80b66c41765e562186608f9eba134e9bd566da2a
Adauto Júnior <adautojunior@opuscm.com.br>
2023-12-14 23:00:20 -0300

    feat: finished lesson 02 of the 'PHP Composer: Dependências, Autoload e Publicação' course

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/src/SearchEngine.php";
+
+use Alura\CoursesSearchEngine\SearchEngine;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(["base_uri" => "https://www.alura.com.br/"]);
+$crawler = new Crawler();
+
+$searchEngine = new SearchEngine($client, $crawler);
+
+$courses = $searchEngine->search("/cursos-online-programacao/php");
+
+foreach ($courses as $course) {
+    echo $course . PHP_EOL;
+}
---
+++ src/SearchEngine.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alura\CoursesSearchEngine;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class SearchEngine {
+    public $httpClient;
+    public $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler) {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function search(string $url): array {
+        $response = $this->httpClient->request("GET", $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $coursesElement = $this->crawler->filter("span.card-curso__nome");
+
+        $courses = [];
+        foreach ($coursesElement as $element) {
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
+}
